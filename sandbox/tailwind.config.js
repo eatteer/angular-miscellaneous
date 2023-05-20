d792960d3997d4dfc28324e87ee078751326a532
Juan Vanegas <eatteer@gmail.com>
2023-05-19 22:21:44 -0500

    feat(tailwind): configure tailwind and daisyui theme

--- sandbox/tailwind.config.js
+++ sandbox/tailwind.config.js
@@ -1,9 +1,26 @@
+const colors = {
+  primary: "#F9D923",
+  "primary-focus": "#DAC132",
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: "tw-",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      ...colors,
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          ...colors,
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
