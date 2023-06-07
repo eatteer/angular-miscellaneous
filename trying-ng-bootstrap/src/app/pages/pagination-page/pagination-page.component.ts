d814ac0a3565fd98c60521442c48e11f9a12d580
Juan Vanegas <eatteer@gmail.com>
2023-06-06 21:17:39 -0500

    feat(trying-nb-bootstrap): fixing ng pagination

--- trying-ng-bootstrap/src/app/pages/pagination-page/pagination-page.component.ts
+++ trying-ng-bootstrap/src/app/pages/pagination-page/pagination-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
+
+const FILTER_PAG_REGEX = /[^0-9]/g;
 
 @Component({
   selector: 'app-pagination-page',
@@ -12,20 +14,17 @@ export class PaginationPageComponent {
 
   public page: number = 1;
 
-  public onPageChange(page: number | string): void {
+  public onPageChange(page: number): void {
     console.log('On page change -> Fetch page', page);
   }
 
   public selectPage(value: string): void {
-    const pageSize = this.pagination.pageSize;
-    const page = this.setPage(Number(value), pageSize);
-    // Trigger manually pageChange event emitter that runs onPageChange
-    this.pagination.pageChange.emit(page);
+    this.page = Number(value);
+    this.pagination.page = this.page;
+    this.pagination.pageChange.emit(this.page);
   }
 
-  public setPage(page: number, pageSize: number): number {
-    if (page > pageSize) return pageSize;
-    if (page < 1) return 1;
-    return page;
+  public formatInput(input: HTMLInputElement) {
+    input.value = input.value.replace(FILTER_PAG_REGEX, '');
   }
 }
